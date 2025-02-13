db45474c5104395e57e71f195e1e59d0f96fd44c
SwekeR-463 <swekerswasti@gmail.com>
2025-02-13 17:18:39 +0530

    day-19

--- day19/bfs.cu
+++ day19/bfs.cu
@@ -4,6 +4,7 @@
 #define BLOCK_QUEUE_SIZE 1024
 #define N 1000000  // e.g. size
 
+// bfs kernel
 __global__ void BFS(unsigned int *p_frontier, unsigned int *p_frontier_tail, 
                                unsigned int *c_frontier, unsigned int *c_frontier_tail, 
                                unsigned int *edges, unsigned int *dest, 
